bdf99cbb3bc17490ad4ec1862399964bdb8ea667
hugomacielads <hugomacielcesar@gmail.com>
2021-02-18 20:10:02 -0300

    Update 72-arrowFunction1.js

--- Funções JS/72-arrowFunction1.js
+++ Funções JS/72-arrowFunction1.js
@@ -1,7 +1,7 @@
 // Sintaxe reduzina, intenção de criar funções rápidas
 
-let dobro = function (a) {
-    return 2 * a
+let dobro = function (a, b) {
+    return 2 * a + b
 }
 
 dobro = (a) => {
@@ -9,7 +9,7 @@ dobro = (a) => {
 }
 
 dobro = a => 2 * a // return implícito
-console.log(dobro(Math.PI))
+console.log(dobro(6))
 
 let ola = function () {
     return 'Olá'
